9fbea9e9e690e01628d300b861fe05347bde15aa
Tigusa <tigusa.r@gmail.com>
2020-11-04 11:46:48 +0100

    Implementierung des Beispielbaumes zum weiteren Testen

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "tree.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    tree::TreePrint(tree::TreeExample1());
 }
 
 MainWindow::~MainWindow()
