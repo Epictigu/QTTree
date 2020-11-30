5fc39ea41ee5519a65bfa5a02e5192645ef4a022
Tigusa <tigusa.r@gmail.com>
2020-11-30 23:15:20 +0100

    Kommandozeilenparameter

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "showknot.h"
 #include "tree.h"
+#include <cstdlib>
+#include <iostream>
 
 #include <QApplication>
 
@@ -10,8 +12,29 @@ int main(int argc, char *argv[])
 
     struct TreeNode* tree = NULL;
     srand (time(NULL));
-    for (int i = 0; i < 10; i++) {
-        int random = rand() % 100;
+
+    int x = 10;
+    int rMi = 0;
+    int rMa = 100;
+
+    if(argc > 0){
+        x = atoi(argv[1]); if (x == 0) x = 10;
+        if(argc > 1){
+            rMi = atoi(argv[2]); if(rMi == 0) rMi = 0;
+            if(argc > 2){
+                rMa = atoi(argv[3]); if(rMa == 0) rMa = 100; rMa -= rMi;
+            }
+        }
+    }
+
+
+    cout << rMi << " - " << rMa << " | " << x;
+    if(rMa < rMi || rMa <= 0 || rMi < 0 || x <= 0){
+        return -1;
+    }
+
+    for (int i = 0; i < x; i++) {
+        int random = (rand() % (rMa + 1)) + rMi;
         tree = tree::TreeInsert(tree, random, "Info zu Schluessel " + std::to_string(random));
     }
 
